d806e679b99a8f8961fcf84a023fa0ed8905b614
clarelimo <clarelimo5@gmail.com>
2021-07-06 15:04:29 +0300

    add data validations to forms

---
+++ School Management/Models/MetaClasses/CourseMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace School_Management.Models
+{
+    public class CourseMetaData
+    {
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Range(1,8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CourseMetaData))]
+    public partial class Course
+    {
+
+    }
+}
